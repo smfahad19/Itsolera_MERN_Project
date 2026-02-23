4d9c84075ed293ca7b39751780e3a3bba18a85cf
Syed Muhammad Fahad <156584040+smfahad19@users.noreply.github.com>
2026-02-23 11:10:18 -0800

    Refactor DB connection and update route imports
    Refactor MongoDB connection logic and update route imports.

--- server/server.js
+++ server/server.js
@@ -1,25 +1,38 @@
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import authRoutes from "./routes/authRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js";
-import sellerRoutes from "./routes/sellerRoutes.js";
-import customerRoutes from "./routes/customerRoutes.js";
+import authRoutes from "../routes/authRoutes.js";
+import adminRoutes from "../routes/adminRoutes.js";
+import sellerRoutes from "../routes/sellerRoutes.js";
+import customerRoutes from "../routes/customerRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log("MongoDB Connected successfully");
+  } catch (error) {
+    console.log("MongoDB Error:", error);
+  }
+};
+
 app.use(
   cors({
     origin: [
-      "https://markethubfront.vercel.app",                 
+      "https://markethubfront.vercel.app",
       "https://markethubfront-mfm4dp0mq-fahads-projects-c5bdce25.vercel.app",
-      "https://markethub-azure.vercel.app",              
-      "https://market-mxq9eu6mp-fahads-projects-c5bdce25.vercel.app" 
+      "https://markethub-azure.vercel.app",
+      "https://market-mxq9eu6mp-fahads-projects-c5bdce25.vercel.app"
     ],
     credentials: true,
   })
@@ -27,10 +40,10 @@ app.use(
 
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB Connected successfully"))
-  .catch((err) => console.log("MongoDB Error:", err));
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Backend Running perfectly");
